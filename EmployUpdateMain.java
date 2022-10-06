f4cb28cb0b962150afae5d62dc8c1a474318c9cd
Akhi <114737969+Akhila143L@users.noreply.github.com>
2022-10-06 16:27:30 +0530

    Add files via upload

---
+++ EmployUpdateMain.java
@@ -0,0 +1,28 @@
+package com.infinite.Jdbc;
+
+import java.sql.SQLException;
+
+import java.util.Scanner;
+
+public class EmployUpdateMain {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	Employ employ = new Employ();
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter employ No  ");
+	employ.setEmpno(sc.nextInt());
+	System.out.println("Enter employ Name  ");
+	employ.setName(sc.next());
+	System.out.println("Enter Department  ");
+	employ.setDept(sc.next());
+	System.out.println("Enter Designation  ");
+	employ.setDesig(sc.next());
+	System.out.println("Enter Basic  ");
+	employ.setBasic(sc.nextInt());
+	try {
+		System.out.println(new EmployDAO().updateEmploy(employ));
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+}
